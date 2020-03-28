84fa60a6a9084e0cf1946374c013c685b7002355
phatpham9 <phatpham9@gmail.com>
2020-03-28 19:01:26 +0700

    feat: replace eslint-config-standard with eslint-config-standard-with-typescript, use @boringcodes/eslint-config for linting this repo

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-module.exports = require('./.eslintrc.js');
+module.exports = {
+  extends: [
+    // https://github.com/standard/eslint-config-standard-with-typescript
+    'standard-with-typescript',
+
+    // https://github.com/prettier/eslint-config-prettier#example-configuration
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/standard',
+  ],
+
+  // https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/README.md#configuration
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+};
